f494c5d86ab733bc23404bd6a5cf8db65e118fc3
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2015-03-12 00:50:08 -0400

    More initial work (ideas, class loader)

---
+++ bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+
+// File:        bootstrap.php
+// Author:      Brian Allen Vanderburg II
+// Purpose:     Bootstrap file for the MyBoard application
+
+namespace MyBoard;
+
+// Set up an autoloader
+spl_autoload_register(function($classname) {
+    $ns = __NAMESPACE__ . '\\';
+    if(substr($classname, 0, strlen($ns)) == $ns) {
+        $classname = substr($classname, strlen($ns));
+        $fname = __DIR__ . '/classes/' . strtolower(str_replace(array('\\', '_'), '/', $classname)) . '.php';
+        if(@file_exists($fname)) {
+            include $fname;
+        }
+    }
+});
+
--- doc/readme.txt
+++ doc/readme.txt
@@ -1,8 +1,9 @@
-MySite - Copyright (C) 2015 Brian Allen Vanderburg II
+MyBoard - Copyright (C) 2015 Brian Allen Vanderburg II
 
-MySite is a personal CMS meant to be used either by itself or along side of
+MyBoard is a personal board/forum meant to be used either by itself or along
 an existing website.  It is primarily intended for use by myself, however is
-made available in case anyone else is interested.
+made available in case anyone else is interested.  It is not to be confused with
+MyBB, which is also a forum software.
 
 Features
 ========
