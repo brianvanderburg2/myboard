d31e1996bcbc9a658c6017fbaa9cba999e388ff1
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2015-04-06 20:46:26 -0400

    Simplified renderer

--- classes/renderer.php
+++ classes/renderer.php
@@ -19,17 +19,6 @@ class Renderer
 {
     protected $board = null;
 
-    /**
-     * This is a map of regular expressions to either a function
-     * or class name.  If the default render method is not overriden
-     * in a base class, this is searched.  If the regular expression
-     * matches, then either the function is called on the current object
-     * if the value of the entry begins with ':', or the class of the value
-     * is constructed and it's render method is called.  In either case
-     * the remaining parameters are stil passed in.
-     */
-    protected static $renderers = array();
-
     public function __construct($board)
     {
         $this->board = $board;
@@ -37,22 +26,13 @@ class Renderer
 
     public function render($params)
     {
-        foreach(static::$renderers as $re => $name)
+        if(count($params))
         {
-            if(preg_match($re, $params[0]))
+            $name = 'render_' . $params[0];
+            if(method_exists($this, $name))
             {
                 array_shift($params);
-
-                if(substr($name, 0, 1) == ':')
-                {
-                    $name = substr($name, 1);
-                    $this->$name($params);
-                }
-                else
-                {
-                    $renderer = new $name($this->board);
-                    $renderer->render($params);
-                }
+                $this->$name($params);
                 return;
             }
         }
