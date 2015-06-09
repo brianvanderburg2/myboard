0dd783b9bac6633c28162cca01d9c15aaa36ad66
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2015-06-09 19:08:51 -0400

    Added data directory to test config.

--- test/config.php
+++ test/config.php
@@ -4,6 +4,9 @@ $config = array(
     'database.host' => '127.0.0.1',
     'database.user' => 'test',
     'database.pass' => 'test',
-    'database.name' => 'test'
+    'database.name' => 'test',
+    
+    # This will become output/test/data 
+    'app.datadir.user' => __DIR__ . "/data"
 );
 
